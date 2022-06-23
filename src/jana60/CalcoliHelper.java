5cbfff8673a42e4e1bc5b0164261150985f0283d
Luniophel <lucarello.stefanoemanuele@gmail.com>
2022-06-23 14:01:52 +0200

    Aggiunte classi CalcoliHelper e CalcoliTest. Aggiunto costruttore private. Aggiunto metodo sum2Integers.

--- src/jana60/CalcoliHelper.java
+++ src/jana60/CalcoliHelper.java
@@ -1,5 +1,26 @@
 package jana60;
 
 public class CalcoliHelper {
+	
+	private CalcoliHelper() {
+		//private Constructor
+	}
+	
+	//METHODS
+	//Calcola la somma di due numeri interi
+	public static int sum2Integers(int num1, int num2) 
+	{
+		return num1 + num2;
+	}
+	
+	//Calcola la differenza tra due numeri interi
+	
+	//Calcola il prodotto di due numeri interi
+	
+	//Calcola il valore assoluto di un numero intero
+	
+	//Calcola il valore minimo tra due interi
+	
+	//Calcola il valore massimo tra due interi
 
 }
