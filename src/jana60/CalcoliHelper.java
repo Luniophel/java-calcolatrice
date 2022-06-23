ae0e341aa4954462ed31841674181c9fd5c4d724
Luniophel <lucarello.stefanoemanuele@gmail.com>
2022-06-23 14:21:56 +0200

    Aggiunti metodi diff2Integers e mult2Integers.

--- src/jana60/CalcoliHelper.java
+++ src/jana60/CalcoliHelper.java
@@ -14,10 +14,22 @@ public class CalcoliHelper {
 	}
 	
 	//Calcola la differenza tra due numeri interi
+	public static int diff2Integers(int num1, int num2) 
+	{
+		return num1 - num2;
+	}
 	
 	//Calcola il prodotto di due numeri interi
+	public static int mult2Integers(int num1, int num2)
+	{
+		return num1 * num2;
+	}
 	
 	//Calcola il valore assoluto di un numero intero
+//	public static int absoluteInt (int num)
+//	{
+//		return
+//	}
 	
 	//Calcola il valore minimo tra due interi
 	
--- src/jana60/CalcoliTest.java
+++ src/jana60/CalcoliTest.java
@@ -3,7 +3,11 @@ package jana60;
 public class CalcoliTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Test del metodo che somma due numeri interi
+		System.out.println(CalcoliHelper.sum2Integers(5, 10));
+		
+		
 
 	}
 
