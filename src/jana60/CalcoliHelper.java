d5e6ade9e8b7b49264d0e29d31fc400f84786b19
Luniophel <lucarello.stefanoemanuele@gmail.com>
2022-06-23 15:16:42 +0200

    Completata consegna base. Iniziato Bonus1, create classi per Double. Corretti nomi metodi coerentemente con la consegna.

--- src/jana60/CalcoliHelper.java
+++ src/jana60/CalcoliHelper.java
@@ -7,32 +7,140 @@ public class CalcoliHelper {
 	}
 	
 	//METHODS
+	
+	//Metodi per Interi
 	//Calcola la somma di due numeri interi
-	public static int sum2Integers(int num1, int num2) 
+	public static int sum(int num1, int num2) 
 	{
 		return num1 + num2;
 	}
 	
 	//Calcola la differenza tra due numeri interi
-	public static int diff2Integers(int num1, int num2) 
+	public static int diff(int num1, int num2) 
 	{
 		return num1 - num2;
 	}
 	
 	//Calcola il prodotto di due numeri interi
-	public static int mult2Integers(int num1, int num2)
+	public static int mult(int num1, int num2)
 	{
 		return num1 * num2;
 	}
 	
 	//Calcola il valore assoluto di un numero intero
-//	public static int absoluteInt (int num)
-//	{
-//		return
-//	}
+	public static int absolute (int num)
+	{
+		if (num >= 0)
+		{
+			return num;
+		}
+		else
+		{
+			return -num;
+		}
+	}
 	
 	//Calcola il valore minimo tra due interi
+	public static int whoMin (int num1, int num2)
+	{
+		if (num1 > num2)
+		{
+			return num2;
+		}
+		else if (num1 < num2)
+		{
+			return num1;
+		}
+		else
+		{
+			System.out.println("ATTENZIONE: Hai dichiarato due numeri identici.");
+			return num1;
+		}
+	}
 	
 	//Calcola il valore massimo tra due interi
+	public static int whoMax (int num1, int num2)
+	{
+		if (num1 > num2)
+		{
+			return num1;
+		}
+		else if (num1 < num2)
+		{
+			return num2;
+		}
+		else
+		{
+			System.out.println("ATTENZIONE: Hai dichiarato due numeri identici.");
+			return num1;
+		}
+	}
+
+	//Metodi per Double
+	public static double sum(double num1, double num2) 
+	{
+		return num1 + num2;
+	}
+	
+	//Calcola la differenza tra due numeri interi
+	public static double diff(double num1, double num2) 
+	{
+		return num1 - num2;
+	}
+	
+	//Calcola il prodotto di due numeri interi
+	public static double mult(double num1, double num2)
+	{
+		return num1 * num2;
+	}
+	
+	//Calcola il valore assoluto di un numero intero
+	public static double absolute (double num)
+	{
+		if (num >= 0)
+		{
+			return num;
+		}
+		else
+		{
+			return -num;
+		}
+	}
+	
+	//Calcola il valore minimo tra due interi
+	public static double whoMin (double num1, double num2)
+	{
+		if (num1 > num2)
+		{
+			return num2;
+		}
+		else if (num1 < num2)
+		{
+			return num1;
+		}
+		else
+		{
+			System.out.println("ATTENZIONE: Hai dichiarato due numeri identici.");
+			return num1;
+		}
+	}
+	
+	//Calcola il valore massimo tra due interi
+	public static double whoMax (double num1, double num2)
+	{
+		if (num1 > num2)
+		{
+			return num1;
+		}
+		else if (num1 < num2)
+		{
+			return num2;
+		}
+		else
+		{
+			System.out.println("ATTENZIONE: Hai dichiarato due numeri identici.");
+			return num1;
+		}
+	}
 
 }
--- src/jana60/CalcoliTest.java
+++ src/jana60/CalcoliTest.java
@@ -4,11 +4,44 @@ public class CalcoliTest {
 
 	public static void main(String[] args) {
 		
-		//Test del metodo che somma due numeri interi
-		System.out.println(CalcoliHelper.sum2Integers(5, 10));
+		//Test del metodo che calcola la somma due numeri interi
+		System.out.println("\n______Calcola Somma______");
 		
+		System.out.println(CalcoliHelper.sum(5, 10));
 		
+		//Test del metodo che calcola la differenza tra due numeri interi
+		System.out.println("\n______Calcola Differenza______");
+		
+		System.out.println(CalcoliHelper.diff(27, 41));	//Minuendo < Sottraendo
+		System.out.println(CalcoliHelper.diff(41, 27));	//Minuendo > Sottraendo
 
+		//Test del metodo che calcola il prodotto di due numeri interi
+		System.out.println("\n______Calcola Prodotto______");
+		
+		System.out.println(CalcoliHelper.mult(4, 9));
+
+		//Test del metodo che calcola il valore assoluto di un numero intero
+		System.out.println("\n______Calcola Valore Assoluto______");
+		
+		System.out.println(CalcoliHelper.absolute(-77));			//Valore negativo
+		System.out.println(CalcoliHelper.absolute(52));			//Valore positivo
+		
+		//Test del metodo che calcola il valore Massimo tra due numeri interi
+		System.out.println("\n______Calcola Massimo______");
+		
+		System.out.println(CalcoliHelper.whoMax(47, 32));	// num1 > num2
+		System.out.println(CalcoliHelper.whoMax(32, 47));	// num1 < num2
+		System.out.println(CalcoliHelper.whoMax(5, 5));	// num1 = num2
+
+		//Test del metodo che calcola il valore Minimo tra due numeri interi
+		System.out.println("\n______Calcola Minimo______");
+		
+		System.out.println(CalcoliHelper.whoMin(47, 32));	// num1 > num2
+		System.out.println(CalcoliHelper.whoMax(32, 47));	// num1 < num2
+		System.out.println(CalcoliHelper.whoMax(5, 5));	// num1 = num2
+
+
+		
 	}
 
 }
